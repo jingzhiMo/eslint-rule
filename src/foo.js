c35478c86d2e47c0b5b443c251caf2be86503eac
jingzhiMo <mojingzhi.mail@gmail.com>
2020-07-05 21:51:00 +0800

    feat: init base file

---
+++ src/foo.js
@@ -0,0 +1,6 @@
+export default () => {
+  // 12312
+  alert(1)
+  alert('2')
+  return 'foo'
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import foo from './foo.js'
+// const foo = require('./foo.js')
+
+/**
+ * @description 简单函数
+ * @params {string} str
+ */
+function helloWorld(str) {
+  return 'hello world!' + str
+}
+// console.log(helloWorld())
+helloWorld()
+foo()
